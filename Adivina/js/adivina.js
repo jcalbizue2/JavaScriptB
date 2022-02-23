71b377038c7ce8e8c6eb856101bbcc936b55709c
jcalbizue2 <juan.carlos.albizu@gmail.com>
2022-02-23 13:17:27 +0100

    indica entre mayor y menor

--- Adivina/js/adivina.js
+++ Adivina/js/adivina.js
@@ -1,40 +1,45 @@
 'use strict';
 
-let objetivo, nintentos;
+let objetivo, intentos, minimo, maximo;
 
 document.addEventListener('DOMContentLoaded', function () {
     const numero = document.getElementById('numero');
     const probar = document.getElementById('probar');
     const respuesta = document.getElementById('respuesta');
-    const intentos = document.getElementById('intentos');
+ 
 
     objetivo = resetear();
+    console.log(objetivo);
 
     probar.addEventListener('click', function () {
         if (objetivo > +numero.value) {
-            respuesta.innerText = 'El objetivo es mayor ' + numero.value;
+            // respuesta.innerText = 'El objetivo es mayor ' + numero.value;
+            minimo = numero.value;
             indicarintentos();
 
         } else if (objetivo < +numero.value) {
-            respuesta.innerText = 'El objetivo es menor ' + numero.value;
+            // respuesta.innerText = 'El objetivo es menor ' + numero.value;
+            maximo = numero.value;
             indicarintentos();
         } else {
             respuesta.innerText = 'Acertaste';
             objetivo = resetear();
+            console.log(objetivo);
         }
     });
 });
 
 function resetear() {
-    nintentos = 0;
+    intentos = 0;
     numero.value = "";
-    intentos.innerText = '';
+    minimo = 1;
+    maximo = 100;
     return parseInt(Math.random() * 100) + 1;    
 }
 
 function indicarintentos(){
     numero.value = "";
-    nintentos += 1;
-    intentos.innerText = 'Lleva ' + nintentos + ' intentos';
+    intentos += 1;
+    respuesta.innerText = 'El objetivo es mayor ' + minimo + ' y menor que ' + maximo +' y llevas ' + intentos + ' intentos';
     numero.focus();
 }
