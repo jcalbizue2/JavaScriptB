0a906e6517e934ecbd0c3652911765ef851d8b01
jcalbizue2 <juan.carlos.albizu@gmail.com>
2022-03-15 18:39:35 +0100

    ipartetify 2

--- ipratekfy/src/App.js
+++ ipratekfy/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import ´Fragment} logo from './logo.svg';
 import './App.css';
+import MenuPrincipal from './MenuPrincipal';
+import MenuSecundarios from './MenuSecundario';
+import Contenido from './Contenido';
+import Alerta from './Alerta';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <MenuPrincipal />
+      <MenuSecundarios />
+      <Contenido />
+      <Alerta />
+    </Fragment>
   );
 }
 
---
+++ ipratekfy/src/MenuPrincipal.js
@@ -0,0 +1,5 @@
+export default function MenuPrincipal(){
+    return(
+        <h1> Menu Principal</h1>
+    )  
+};
